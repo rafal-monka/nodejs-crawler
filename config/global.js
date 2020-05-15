7a128e695a561d8dfacdbbfbfb797614d5a4eb0c
Rafal Monka <monka.rafal@gmail.com>
2020-05-15 20:31:06 +0200

    count

--- config/global.js
+++ config/global.js
@@ -1,7 +1,7 @@
 exports.scanTables = [
-    {lang: 'ENG', category: 55, price_from: 49, price_to: 50},
-    {lang: 'POL', category: 55, price_from: 49, price_to: 50},
-    {lang: 'POL', category: 55, price_from: 40, price_to: 50}
+    {lang: 'ENG', category: 55, price_from: 49, price_to: 50, done: 0, count: null},
+    {lang: 'POL', category: 55, price_from: 49, price_to: 50, done: 0, count: null},
+    {lang: 'POL', category: 55, price_from: 40, price_to: 50, done: 0, count: null}
 ]
 
 exports.currentScan = 0;
